564e4ca950492ac2c7ee6a235a8aae146b628061
openkitx403 <openkitx403@outlook.com>
2025-11-06 17:14:49 +0000

    updated ts client

--- demo-typescript/server/server.ts
+++ demo-typescript/server/server.ts
@@ -25,7 +25,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Openkitx403-Signature', 'X-Openkitx403-Timestamp', 'X-Openkitx403-Address', 'WWW-Authenticate']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Openkitx403-Signature', 'X-Openkitx403-Timestamp', 'X-Openkitx403-Address', 'WWW-Authenticate', 'www-authenticate']
 }));
 
 app.use(express.json());
